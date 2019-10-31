59d0909de6808ffd77d08c379294d04357f0b034
Livany <40469622+livanynunes@users.noreply.github.com>
2019-10-31 15:04:06 -0300

    Fix starvation problem

--- src/travessia/TrafficController.java
+++ src/travessia/TrafficController.java
@@ -4,34 +4,44 @@ public class TrafficController {
 
 	private int nred = 0; // number of red cars on bridge
 	private int nblue = 0; // number of blue cars on bridge
+	private int esperaAzul = 0; //conta o numero de carros azuis que estão na espera
+	private int esperaVermelho = 0; // numero de carros vermelhos esperando 
+	private boolean vezAzul = true;
 	
 	//monitor invariant: nred>= 0 and nblue>= 0 and not
 	// (nred>0 and nblue>0)
 	
-	// Carros vermelhos passarem de uma vez é coincidência 
+	 
     public synchronized void entradaCarroVermelho() throws InterruptedException{
-    		while(nblue > 0) {
-    			wait(); // trava
+    	++esperaVermelho;
+    	
+    	while(nblue > 0 || (esperaAzul > 0 && vezAzul) ) {
+    			wait(); // aguarda
     		}
+    		--esperaVermelho;
     		++nred;
     }
     
     public synchronized void saidaCarroVermelho() {
     	--nred;
+    	vezAzul = true;
     	if(nred ==0) {
-    		notifyAll(); //destrava
+    		notifyAll(); //prossegue
     	}
     }
     
     public synchronized void entradaCarroAzul() throws InterruptedException {
-    	while(nred>0) {
-    		wait(); // trava
+    	++esperaAzul;
+    	while(nred>0 || (esperaVermelho>0 && !vezAzul) ) {
+    		wait(); // aguarda
     	}
+    	--esperaAzul;
     	++nblue;
     }
     
     public synchronized void saidaCarroAzul() {
     	--nblue;
+    	vezAzul = false;
     	if(nblue ==0) {
     		notifyAll(); // destrava
     	}
